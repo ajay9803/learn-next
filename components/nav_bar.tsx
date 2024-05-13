6a415db0bd21370fd5f786733b6165a3a549644f
ajay9803 <ajayprajapati9803@gmail.com>
2024-05-13 22:48:17 +0545

    Configure tailwind css

--- components/nav_bar.tsx
+++ components/nav_bar.tsx
@@ -1,24 +1,15 @@
+import Link from 'next/link';
 import React from 'react'
-import styles from "../styles/navbar.module.css";
 
 const NavBar = () => {
-    return (
-        <div className={styles.navBar}>
-            <div className={styles.firstSection}>
-                <h1 className={styles.title}> Blog Spot. </h1>
-                <nav>
-                    <ul className={styles.navList}>
-                        <li className={styles.navTitle}> Articles </li>
-                        <li className={styles.navTitle}> Podcast </li>
-                        <li className={styles.navTitle}> Talk to Us</li>
-                    </ul>
-                </nav>
-            </div>
-            <div className={styles.secondSection}>
-                <button className={styles.menuButton}> <p className={styles.buttonContent}> Menu </p> </button>
-            </div>
+    return <div className='flex flex-row '>
+        <div className='flex flex-row py-6 justify-center gap-x-8 relative w-full'>
+            <Link href={'/'} className='font-normal text-sm hover:font-semibold hover:text-red-600 cursor-pointer hover:border-b-2 hover:border-red-600 transition-all duration-500 ease-out'> About </Link>
+            <Link href={'/'} className='font-normal text-sm hover:font-semibold hover:text-red-600 cursor-pointer hover:border-b-2 hover:border-red-600 transition-all duration-500 ease-out'> Learn More </Link>
+            <Link href={'/'} className='font-normal text-sm hover:font-semibold hover:text-red-600 cursor-pointer hover:border-b-2 hover:border-red-600 transition-all duration-500 ease-out'> Contact </Link>
+            <Link href={'/'} className='font-normal text-sm hover:font-semibold hover:text-red-600 cursor-pointer hover:border-b-2 hover:border-red-600 transition-all duration-500 ease-out'> Resources </Link>
+            <div className='-z-10 absolute h-96 w-96 bg-red-600 rounded-full -left-24 -top-64 rotate-12'></div>
         </div>
-    )
+    </div>
 }
-
 export default NavBar;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,5 @@
-import NavBar from "@/components/nav_bar";
-import Head from "next/head";
-import Script from "next/script";
-import { Fragment } from "react";
-
-
 const HomePage = (props: any) => {
-
-  return (
-    <Fragment>
-
-      {/* set up headers */}
-      <Head>
-        <title> Blog App </title>
-        <meta name="description" content="this is description."></meta>
-      </Head>
-
-      {/* set up scripts outside head */}
-      <Script id="1" strategy="lazyOnload">
-        {/* alert('Hello world'); */}
-      </Script>
-    </Fragment>
-  );
+  return <p> </p>
 };
 
-export default HomePage;        
+export default HomePage;
